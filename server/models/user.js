3a1db5ccf6e01ad548ef572d1f633ebb1a5a3288
Ricardo Fideles <ricardo@ricardofideles.com.br>
2017-07-11 17:44:45 -0300

    add post /users route and token method

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,60 @@
-var mongoose = require('mongoose');
 
-var User = mongoose.model('User', {
+const mongoose = require('mongoose');
+const validator = require('validator');
+//const {SHA256} = require('crypto-js');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
+
+
+var UserSchema = new mongoose.Schema({
     email : {
         type: String,
         required: true,
+        trim: true,
         minlenght: 1,
-        trim: true
-    }
+        unique: true, 
+        validate : {
+            validator : validator.isEmail, 
+            message : '{VALUE}, invalid e-mail'
+        }
+    },
+    password : {
+        type : String,
+        require : true,
+        minlenght : 6
+    },
+    tokens : [{
+        access : {
+            type: String,
+            require: true
+        },
+        token : {
+            type: String,
+            require: true
+        }
+    }]
 });
+
+UserSchema.methods.toJSON = function() {
+    var user = this;
+    var userObject = user.toObject();
+
+    return _.pick(userObject, ['_id', 'email']);
+}
+
+UserSchema.methods.generateAuthToken = function () {
+    var user = this;
+    var access = 'auth';
+    var token = jwt.sign({_id : user._id.toHexString(), access}, access).toString();
+    user.tokens.push({
+        access,
+        token
+    });
+    return user.save().then(() => {
+        return token;
+    });
+};
+
+var User = mongoose.model('User', UserSchema);
 
 module.exports =  {User};
